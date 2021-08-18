a9120ee3de35bab153a225d2b6ad9c5d52abb378
abinalexpothen <abinalexpothen@gmail.com>
2021-08-17 23:11:03 -0400

    feat: Add drive_bot node
    DriveToTarget service is used to command linear and angular velocities.

---
+++ ball_chaser/src/drive_bot.cpp
@@ -0,0 +1,42 @@
+#include "ros/ros.h"
+#include "ball_chaser/DriveToTarget.h"
+#include "geometry_msgs/Twist.h"
+
+// Global joint publisher objects
+ros::Publisher motor_command_publisher;
+
+bool handle_drive_request(ball_chaser::DriveToTarget::Request &req, ball_chaser::DriveToTarget::Response &res)
+{
+    ROS_INFO("DriveToTargetRequest received - linear_x: %1.4f, angular_z: %1.4f", (float)req.linear_x, (float)req.angular_z);
+
+    geometry_msgs::Twist motor_command;
+
+    motor_command.linear.x = req.linear_x;
+    motor_command.angular.z = req.angular_z;
+
+    motor_command_publisher.publish(motor_command);
+
+    res.msg_feedback = "Target request set. linear_x: " + std::to_string(req.linear_x) + " , angular_z: " + std::to_string(req.angular_z);
+    ROS_INFO_STREAM(res.msg_feedback);
+
+    return true;
+}
+
+int main(int argc, char** argv)
+{
+    // init drive_bot node and create a handle
+    ros::init(argc, argv, "drive_bot");
+    ros::NodeHandle n;
+
+    // define publishers
+    motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+
+    // define command_robot service with handle_command_robot_request callback
+    ros::ServiceServer service = n.advertiseService("ball_chaser/command_robot", handle_drive_request);
+
+    ROS_INFO("Ready to send target commads");
+
+    ros::spin();
+
+    return 0;
+}
